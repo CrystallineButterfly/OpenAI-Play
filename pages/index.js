3477ecfe25caf93dafb5458039c32749b2f69afe
Kell McWatt <fromtc2k42@gmail.com>
2023-02-10 23:31:12 +0000

    Adjust Ui v1

--- pages/index.js
+++ pages/index.js
@@ -35,11 +35,11 @@ export default function Home() {
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <link rel="icon" href="/butterfly.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
+        <img src="/butterfly.png" className={styles.icon} />
         <h3>Name my pet</h3>
         <form onSubmit={onSubmit}>
           <input
